77a8a7d2a6d91f39583bb3a25488129f2519e64a
zac <isaacmubiru99@gmail.com>
2022-07-06 12:55:23 +0300

    fix: stringfy key words

--- src/controllers/tourController.ts
+++ src/controllers/tourController.ts
@@ -6,6 +6,7 @@ import { uploadImageToStorage } from "./fileController";
 
 export const createTour = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
+    console.log("keyworsds",req.body )
     if (req.body.dayActivityDescription) {
       req.body.dayActivityDescription = JSON.parse(
         req.body.dayActivityDescription
